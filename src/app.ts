f5bde10d8d23672c19d3aec71ecd3eb0ff69b6b0
Đạt Lê Thành <datlt.uit@gmail.com>
2018-12-06 08:53:48 +0700

    remove view

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,18 @@
 import express from "express";
 import compression from "compression";  // compresses requests
-import session from "express-session";
 import bodyParser from "body-parser";
 import lusca from "lusca";
 import dotenv from "dotenv";
-import mongo from "connect-mongo";
-import flash from "express-flash";
-import path from "path";
 import mongoose from "mongoose";
 import passport from "passport";
 import expressValidator from "express-validator";
 import bluebird from "bluebird";
-import { MONGODB_URI, SESSION_SECRET } from "./util/secrets";
-
-const MongoStore = mongo(session);
+import { MONGODB_URI } from "./util/secrets";
 
 // Load environment variables from .env file, where API keys and passwords are configured
 dotenv.config();
 
 // Controllers (route handlers)
-import * as homeController from "./controllers/home";
 import * as userController from "./controllers/user";
 
 // Create Express server
@@ -38,58 +31,17 @@ mongoose.connect(mongoUrl, { useCreateIndex: true, useNewUrlParser: true }).then
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
-app.use(session({
-    resave: true,
-    saveUninitialized: true,
-    secret: SESSION_SECRET,
-    store: new MongoStore({
-        url: mongoUrl,
-        autoReconnect: true
-    })
-}));
 app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
-app.use((req, res, next) => {
-    res.locals.user = req.user;
-    next();
-});
-
-app.use((req, res, next) => {
-    // After successful login, redirect back to the intended page
-    if (!req.user &&
-        req.path !== "/login" &&
-        req.path !== "/signup" &&
-        !req.path.match(/^\/auth/) &&
-        !req.path.match(/\./)) {
-        req.session.returnTo = req.path;
-    } else if (req.user &&
-        req.path == "/account") {
-        req.session.returnTo = req.path;
-    }
-    next();
-});
-
-app.use(
-    express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
-);
 
 /**
  * Primary app routes.
  */
-app.get("/", homeController.index);
-app.get("/login", userController.getLogin);
-app.post("/login", userController.postLogin);
-app.get("/register", userController.getRegister);
-app.post("/register", userController.postRegister);
-app.get("/logout", userController.logout);
+
 
 export default app;
